95c4345969c17e173e0af7f5a519007df367f848
Laura Buaillon <laura.buaillon1@gmail.com>
2025-12-08 11:22:56 +0100

    exercice fin

--- exercice_10/script.js
+++ exercice_10/script.js
@@ -1,15 +1,17 @@
 const champtexte = document.getElementById("filtreAnimaux");
 const liste = document.querySelectorAll(".animal");
 
+ //Cet événement se déclenche À CHAQUE FOIS que l'utilisateur tape ou efface un caractère
 champtexte.addEventListener("input", (e) => {
   let texteRecherche = e.target.value; //récupère ce que l'utilisateur a tapé
+                                       //e.target.value contient le texte actuel du champ
 
-  liste.forEach((li) => {
-    let texteAnimal = li.textContent;
-    if (texteAnimal.includes(texteRecherche)) {
-      li.style.display = "";
+  liste.forEach((li) => {              //faire une boucle pour vérifier chaque élément de la liste
+    let texteAnimal = li.textContent;  //je récupère le texte de l'animal actuel
+    if (texteAnimal.includes(texteRecherche)) { //je vérifie si le texte de l'animal contient le texte recherché
+      li.style.display = "";           //"" affiche par défaut (visible)
     } else {
-      li.style.display = "none";
+      li.style.display = "none";        //none= l'élement devient invisible
     }
   });
 });
